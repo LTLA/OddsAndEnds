61a338129ecf56ed0ffe22006814cf5feeb4e5b6
Aaron Lun <alun@wehi.edu.au>
2015-06-17 20:23:41 +1000

    Got rid of etc/Makeconf modification.

--- bioctools/check_all.sh
+++ bioctools/check_all.sh
@@ -79,27 +79,8 @@ ELAPSED=`echo "scale=8; ($END - $START) / 1000000000" | bc`
 echo "Elapsed time for CHECK: $ELAPSED seconds" >> $logfile
 echo >> $logfile
 
-# Installing with warnings attached, if possible.
-locale=`echo 'cat(paste0(R.home(), "\n"))' | $RCMD --no-save --vanilla --silent | grep -v ">"`
-confile=$locale/etc/Makeconf
-newconf=${confile}.temp
-if [ -w $confile ]
-then
-	if [ ! -e $newconf ]
-	then
-		# If we got interrupted last time before we could do the final move, we don't want to overwrite
-		# the '.temp' file with our new settings. So, we don't copy if '.temp' already exists.
-		cp $confile $newconf
-	fi
-	cat $newconf | sed "s/^CXXFLAGS.*/CXXFLAGS = -g -O0 \$(LTO) -Wall -Wextra -pedantic/" > $confile
-	$RCMD CMD INSTALL $tarball >> $logfile 2>&1
-	mv $newconf $confile
-else
-	printf "WARNING: failed to modify GCC compilation flags, building as usual instead...\n\n" >> $logfile
-	$RCMD CMD INSTALL $tarball >> $logfile 2>&1
-fi
-
 # Running custom tests.
+$RCMD CMD INSTALL $tarball >> $logfile 2>&1
 cd ${packname}.Rcheck/${packname}/tests
 if [[ $useVG -eq 1 ]]
 then
