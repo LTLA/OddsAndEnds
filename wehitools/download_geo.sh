6c7a4093e4ffb0832cf7cfc13991ac02c77ddbf7
Aaron Lun <alun@wehi.edu.au>
2016-02-13 16:09:46 +0000

    Updated script to clean up after itself.

--- wehitools/download_geo.sh
+++ wehitools/download_geo.sh
@@ -3,4 +3,5 @@ do
     firstthree=$(echo $x | cut -c 1-6)
     ascp -i /usr/local/bioinfsoftware/aspera/current/etc/asperaweb_id_dsa.openssh -k1 -Tr -l100m anonftp@ftp.ncbi.nlm.nih.gov:sra/sra-instant/reads/ByRun/sra/SRR/${firstthree}/$x .
     mv $x/* .
+    rmdir $x
 done
