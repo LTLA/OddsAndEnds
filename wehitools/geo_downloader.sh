656c088be4cbc8a85e98411a8ef96a700f818932
Aaron Lun <alun@wehi.edu.au>
2016-02-13 16:09:06 +0000

    Added script to download files from GEO.

---
+++ wehitools/geo_downloader.sh
@@ -0,0 +1,6 @@
+for x in $@
+do 
+    firstthree=$(echo $x | cut -c 1-6)
+    ascp -i /usr/local/bioinfsoftware/aspera/current/etc/asperaweb_id_dsa.openssh -k1 -Tr -l100m anonftp@ftp.ncbi.nlm.nih.gov:sra/sra-instant/reads/ByRun/sra/SRR/${firstthree}/$x .
+    mv $x/* .
+done
